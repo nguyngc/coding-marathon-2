68fbe11df444248e14e8bcce8b49948e8061e65b
Hoang-Vuu <Hoang.Vu3@metropolia.fi>
2025-11-28 10:54:34 +0200

    Iteration 3

--- backend/api-server-starter/app.js
+++ backend/api-server-starter/app.js
@@ -1,27 +1,29 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-// const userRouter = require("./routes/userRouter");
-const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
-const connectDB = require("./config/db");
 const cors = require("cors");
 
-// Middlewares
-app.use(cors())
+const connectDB = require("./config/db");
+const { unknownEndpoint, errorHandler } = require("./middleware/customMiddleware");
+
+const userRouter = require("./routes/userRouter");  
+const jobRouter = require("./routes/jobRoutes");  
+
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
 connectDB();
 
-// Use the userRouter for all /users routes
-// app.use("/api/users", userRouter);
+app.use("/api/users", userRouter);
+
+app.use("/api/jobs", jobRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
 const port = process.env.PORT || 4000;
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
