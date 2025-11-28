a8e5378fff0b58e6e7901998ded60138daf52114
Ngoc Nguyen <ngoc.nguyen15@metropolia.fi>
2025-11-28 14:29:06 +0200

    Refactor useLogin to use new custom hook useFetch

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,31 +1,41 @@
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify"; 
+import { toast } from "react-toastify";
+import useFetch from "./useFetch";
 
 const useLogin = (setIsAuthenticated, email, password) => {
   const navigate = useNavigate();
 
+  const { execute, loading } = useFetch("/api/users/login", {}, true);
+
   const handleLogin = async (e) => {
     e.preventDefault();
+
     try {
-      const response = await fetch("/api/users/login", {
+      // const response = await fetch("/api/users/login", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({ email, password }),
+      // });
+      // const data = await response.json();
+      // console.log(data);
+
+      const data = await execute({
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ email, password })
       });
-      const data = await response.json();
-      console.log(data);
-      
-      if (response.ok) {
-        sessionStorage.setItem("user", JSON.stringify(data));
-        toast.success("Login successful!");
-        setIsAuthenticated(true);
-        navigate("/");
-      } else {
-        toast.error("Login failed: " + data.error);
-      }
+
+      // if (response.ok) {
+      sessionStorage.setItem("user", JSON.stringify(data));
+      toast.success("Login successful!");
+      setIsAuthenticated(true);
+      navigate("/");
     } catch (error) {
-      toast.error("Error during login:" + error);
+      toast.error("Login failed: " + error.message);
     }
+    // } catch (error) {
+    //   toast.error("Error during login:" + error);
+    // }
   };
 
   return { handleLogin };
